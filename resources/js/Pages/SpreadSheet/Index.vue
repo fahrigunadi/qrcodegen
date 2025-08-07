4785a746fb544a4a38c301c17d04ad24113d39e4
Fahri Gunadi <withfahri@gmail.com>
2025-08-07 15:59:29 +0700

    reset btn

--- resources/js/Pages/SpreadSheet/Index.vue
+++ resources/js/Pages/SpreadSheet/Index.vue
@@ -4,18 +4,24 @@
       <form v-on:submit.prevent="onSubmit" class="flex-1">
         <div>
           <label for="link" class="block mb-2 text-sm font-medium text-gray-900">link</label>
-          <input ref="inputEl" type="url" v-model="link" id="link" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="https://example.com" required />
+          <input ref="inputEl" type="url" v-model="link" id="link"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="https://example.com" required />
         </div>
 
         <div class="mt-3">
           <label for="tab" class="block mb-2 text-sm font-medium text-gray-900">Tab</label>
-          <input type="text" v-model="tab" id="tab" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Some tab" required />
+          <input type="text" v-model="tab" id="tab"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="Some tab" required />
         </div>
-  
+
         <button type="submit"
           class="text-gray-900 mt-3 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Generate</button>
+        <button type="button" v-on:click="reset"
+          class="text-gray-900 mt-3 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Reset</button>
       </form>
-  
+
       <QrPreview :base64Result="base64Result" :variant="variant" @variant="variant = $event" :filename="filename" />
     </div>
   </Main>
@@ -54,12 +60,18 @@ watch(variant, async (val) => {
 })
 
 watch(tab, async (val) => {
-    if (val) {
-        filename.value = `qr-tab-${val}.png`
-    } else {
-        filename.value = 'qrcode.png'
-    }
+  if (val) {
+    filename.value = `qr-tab-${val}.png`
+  } else {
+    filename.value = 'qrcode.png'
+  }
 })
+
+const reset = () => {
+  link.value = ''
+  tab.value = ''
+  base64Result.value = ''
+}
 
 const onSubmit = async () => {
   if (!link.value) {
